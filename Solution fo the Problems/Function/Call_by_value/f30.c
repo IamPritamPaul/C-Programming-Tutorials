b206b1eb8987838a680f1d5564d5a725943c9831
IamPritamPaul <paul.pritamg3@gmail.com>
2024-02-10 23:47:10 +0530

    updated staging branch

--- Solution fo the Problems/Function/Call_by_value/f30.c
+++ Solution fo the Problems/Function/Call_by_value/f30.c
@@ -1,6 +1,24 @@
 // 30. Write a program to input any number and check it is prime or not using call by value with return.
+// need mofication
 #include <stdio.h>
 #include <math.h>
+int my_sqrt(int x)
+{
+    for (int i = 2; i < x; i++)
+    {
+        if (x / i == i)
+        {
+            return i;
+        }
+        else
+        {
+            if ((x - i - i / 2) / i == i)
+            {
+                return i;
+            }
+        }
+    }
+}
 int check_factor(int x)
 {
     int i = 2, count = 0;
@@ -16,8 +34,9 @@ int check_prime(int n)
 {
     if (n == 1)
         return 0;
-    int w = sqrt(n);
-    printf("%d\n", check_factor(w));
+    int w = my_sqrt(n);
+    printf("%d is the sqrt.\n", w);
+    // printf("%d\n", check_factor(w));
     return (check_factor(w) == 0);
 }
 int main()
@@ -35,3 +54,42 @@ int main()
     }
     return 0;
 }
+
+/*
+int my_sqrt(int x)
+{
+  for(int i=2;i<x;i++)
+  {
+    if(x/i==i)
+    {
+       return i;
+    }
+    else
+    {
+        if((x-i-i/2)/i==i)
+        {
+        return i;
+        }
+    }
+  }
+}
+int check_prime(int x)
+{
+    int sum=0;
+    if(x==1)
+    {
+      sum++;
+      return sum;
+    }
+    else{
+    for(int i=2;i<my_sqrt(x);i++)
+    {
+      if(x%i==0)
+      {
+        sum++;
+      }
+    }
+    return sum;
+    }
+}
+*/
