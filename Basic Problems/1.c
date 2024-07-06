c9bb5f61894782a865b7eb65b4fb8aa1ff2ea5bc
IamPritamPaul <paul.pritamg3@gmail.com>
2024-07-06 17:20:31 +0530

    new codes added

---
+++ Basic Problems/1.c
@@ -0,0 +1,8 @@
+// Write a program to print 3 names in 3 different lines.
+#include <stdio.h>
+
+int main()
+{
+    printf("Name1\nName2\nName3\n");
+    return 0;
+}
---
+++ Basic Problems/2.c
@@ -0,0 +1,12 @@
+// Write a program to print the sum, difference, multiplication, division of two numbers.
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20;
+    printf("%d+%d=%d\n", a, b, a + b);
+    printf("%d-%d=%d\n", a, b, a - b);
+    printf("%d*%d=%d\n", a, b, a * b);
+    printf("%d//%d=%d\n", a, b, a / b);
+    return 0;
+}
